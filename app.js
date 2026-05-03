4d79e43f8b1fe6ddd80b2f231f921f8f51a0fdbe
Tahoshin Islam <171826325+TahoshinIslam@users.noreply.github.com>
2026-05-03 21:29:42 +0600

    Change CORS origin to production URL
    Updated CORS origin to use production URL.

--- app.js
+++ app.js
@@ -11,16 +11,7 @@ const router = require("./backend/src/routes/api");
 
 const app = express();
 
-app.set("trust proxy", 1);
-
-app.use(cookieParser());
-app.use(helmet());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  }),
-);
+app.set("trust proxy", 1); app.use(cookieParser()); app.use(helmet()); app.use( cors({ origin: process.env.CLIENT_URL || "https://mernstack-portfolio-frontend.onrender.com", credentials: true, }), );
 app.use(mongoSanitize());
 app.use(hpp());
 app.use(express.json({ limit: "10mb" }));
